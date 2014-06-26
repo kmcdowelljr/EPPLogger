97af5be68b57381560935f6ad850ffe2e000da63
kmcdowelljr <keith.mcdowell@neustar.biz>
2014-06-26 14:03:05 -0400

    First update

---
+++ EPP Logger/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace EPP_Logger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MainWindow_fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputText_Box = new System.Windows.Forms.TextBox();
+            this.updateProgressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MainWindow_fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1325, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MainWindow_fileToolStripMenuItem
+            // 
+            this.MainWindow_fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.MainWindow_fileToolStripMenuItem.Name = "MainWindow_fileToolStripMenuItem";
+            this.MainWindow_fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.MainWindow_fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(13, 39);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputBox.Size = new System.Drawing.Size(490, 464);
+            this.InputBox.TabIndex = 1;
+            // 
+            // OutputText_Box
+            // 
+            this.OutputText_Box.Location = new System.Drawing.Point(770, 39);
+            this.OutputText_Box.Multiline = true;
+            this.OutputText_Box.Name = "OutputText_Box";
+            this.OutputText_Box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputText_Box.Size = new System.Drawing.Size(530, 464);
+            this.OutputText_Box.TabIndex = 2;
+            // 
+            // updateProgressBar
+            // 
+            this.updateProgressBar.Location = new System.Drawing.Point(13, 509);
+            this.updateProgressBar.Name = "updateProgressBar";
+            this.updateProgressBar.Size = new System.Drawing.Size(1300, 23);
+            this.updateProgressBar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 541);
+            this.Controls.Add(this.updateProgressBar);
+            this.Controls.Add(this.OutputText_Box);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Neustars, Inc ©™ EPP Logger";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MainWindow_fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputText_Box;
+        private System.Windows.Forms.ProgressBar updateProgressBar;
+    }
+}
+
